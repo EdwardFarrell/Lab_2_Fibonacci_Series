29c6202048dba642dd08a8de5e71b120f0c0a1f3
EdwardFarrell <127593470+EdwardFarrell@users.noreply.github.com>
2023-05-06 01:40:32 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
-#include "mbed.h"
+/* This program is used to find the
+ Fibonacci series using recursion. In recursion a function fib will call itself
+ again and again to form the fiboncci series*/
 #include "C12832.h"
-
+#include "mbed.h"
 
 C12832 lcd(p5, p7, p6, p8, p11);
 
-int main()
-{
-    int j=0;
-    lcd.cls();
-    lcd.locate(0,3);
-    lcd.printf("mbed application board!");
+int n = 20; // For exsmple it is to find fibonacci series of first 12 numbers
 
-    while(true) {   // this is the third thread
-        lcd.locate(0,15);
-        lcd.printf("Counting : %d",j);
-        j++;
-        wait(1.0);
-    }
+int fib(int n); // fibonnaci function is declared as fib()
+
+int main() {
+  lcd.cls();
+
+  lcd.printf("Lab 2 program Fib Series \r\n");
+
+  for (int i = 0; i < n; i++) {
+    lcd.printf("%d, ", fib(i));
+  }
+  lcd.printf("..!\r\n");
+}
+
+// Fibonnaci Function 
+
+int fib(int n) {
+  // take an input n and calculate part of the fibonaci series and return
+  // result.
+  if (n == 0)
+    return 0; // special case if n = 0, fibonacci series is 0
+  if (n == 1)
+    return 1; // special case if n = 1, fibonacci series is 1
+  else {
+    return (fib(n - 1) + fib(n - 2)); // past two values in the series are added
+                                      // to determine the present value
+  }
 }
